0f22b49347d36ccd57d68a43ba4af4c0cb89e3e1
lucas Ferreira <lucaslfr00@gmail.com>
2024-03-07 17:52:32 -0300

    Iniciando o projeto de crianção de um banco, rodas de criação de conta finalizadas

--- api-ignite/src/birds.js
+++ api-ignite/src/birds.js
@@ -1,18 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
-router.get('/', function(req, res) {
-    
-    return res.json({message: "hello word"})
+const customers = []
+
+router.get('/', (req, res)=>{
+    return res.json({message: "bem vindo ao banco UL"})
 })
 
-router.get('/courses', (req, res)=>{
-    return res.json({cursos: {
-        1: "curso1",
-        2: "curso2",
-        3: "curso3"
-    }})
-}) 
+router.get('/account/', (req, res)=>{
+    const {id} = req.params
+    return res.json(customers[id] || customers)
+})
+
+router.post('/account', (req, res)=>{
+    const {cpf, name} = req.body;
+
+    const customersExist = customers.some( (customer) => customer.cpf === cpf)
+
+    if(customersExist){
+        return res.status(400).json({Error: "Cpf já cadastrado"})
+    }
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return res.status(201).send()
+})
+
+router.get('/statement', (req, res)=>{
+    const {cpf} = req.headers;
+
+    const customer = customers.find( (customer) => customer.cpf === cpf)
+
+    if (!customer) {
+        return res.status(400).json({Error: "usuário não existe"})
+    }
+
+    res.json(customer.statement)
+})
 
 module.exports = router;
 
--- api-ignite/src/index.js
+++ api-ignite/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bird = require('./birds.js')
 
 const app = express();
+app.use(express.json())
 
 app.use('/', bird)
 app.use('/courses', bird)
